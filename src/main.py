e31d597027a41f95d90214f5a1b4658f27a327e0
SKRANTZ5 <SKRANTZ5@volvocars.com>
2024-01-12 13:19:09 +0100

    fixed for assignment 1

---
+++ src/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 13:14:57 2024
+
+@author: SKRANTZ5
+"""
+from src.routes import signal_interpreter_app
+
+def main():
+    signal_interpreter_app.run()
+
+if __name__ == "__main__":
+    main()
--- src/routes.py
+++ src/routes.py
@@ -7,16 +7,16 @@ Created on Fri Jan 12 10:33:09 2024
 
 from flask import Flask, request
 
-my_app = Flask(__name__)
+signal_interpreter_app = Flask(__name__)
 
-@my_app.route("/", methods=["GET"])
-def hello():
-    return "Hello World!"
+# @signal_interpreter_app.route("/", methods=["GET"])
+# def hello():
+#     return "Hello World!"
 
 
-@my_app.route("/", methods=["POST"])
+@signal_interpreter_app.route("/", methods=["POST"])
 def mirror_data():
     data = request.get_json()
     return data
 
-my_app.run()
+signal_interpreter_app.run()
