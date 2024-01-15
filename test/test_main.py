f6b06566c2cba4f23841c914f12836d9b10473be
SKRANTZ5 <SKRANTZ5@volvocars.com>
2024-01-15 14:51:50 +0100

    solved main test

--- test/test_main.py
+++ test/test_main.py
@@ -6,7 +6,8 @@ Created on Mon Jan 15 10:39:55 2024
 """
 
 from unittest.mock import patch
-from src.main import parse_arguments, ArgumentParser
+from src.main import parse_arguments, ArgumentParser, main
+from src.json_parser import JsonParser
 
 class MockArgs:
     file_path = "C:\signal_database.json"  # Fick lägga på c disk pga volvo strula upp med syntaxen på mapparna
@@ -18,8 +19,10 @@ def test_parse_arguments(mock_parse_args, mock_add_argument):
     mock_parse_args.assert_called_once()
     mock_add_argument.assert_called_with("--file_path")
 
-# @patch.object(JsonParser, "load_file")
-# def test_main(mock_load_file):
-#     mock_load_file.assert_called_once()
-#     mock_load_file.assert_called_with(MockArgs.file_path)
+@patch("src.main.parse_arguments", return_value=MockArgs)
+@patch.object(JsonParser, "load_file")
+def test_main(mock_load_file, mock_parse_arguments):
+    main()
+    mock_load_file.assert_called_once()
+    mock_load_file.assert_called_with(MockArgs.file_path)
     
