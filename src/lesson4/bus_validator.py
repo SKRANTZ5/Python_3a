4c85e7b98fa5dc01f9926ed2d62bc795cec0dc95
SKRANTZ5 <SKRANTZ5@volvocars.com>
2024-01-17 10:01:54 +0100

    exc 3 done

--- src/lesson4/bus_validator.py
+++ src/lesson4/bus_validator.py
@@ -4,17 +4,26 @@ Created on Wed Jan 17 08:50:18 2024
 
 @author: SKRANTZ5
 """
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 def bus_validator():
-    age = input("Gimme your age: ")
     try:
-        age = int(age)
+        age = int(input("Gimme your age: "))
+        logger.info("User gave age: %d", age)
     except ValueError as err:
         print("You need to enter a number")
+        logger.exception("Exception occurred %s", err)
     else:
         if age >= 24:
             print("Congratulations, you are old enough to drive a bus")
-        elif age <= 23:
+        else:
             print(f"Sorry, you have to wait {24-age} more years")
     finally:
         print("Good bye")
+        
+if __name__ == "__main__":
+    bus_validator()
+        
--- test/test_bus_validator.py
+++ test/test_bus_validator.py
@@ -5,3 +5,26 @@ Created on Wed Jan 17 08:59:06 2024
 @author: SKRANTZ5
 """
 
+from unittest.mock import patch, call
+from src.lesson4.bus_validator import bus_validator
+
+@patch("src.lesson4.bus_validator.logger")
+@patch("builtins.print")
+@patch("builtins.input", return_value=25)
+def test_bus_validator_above_24(mock_input, mock_print, mock_logger):
+    bus_validator()
+    assert mock_print.mock_calls == [call("Congratulations, you are old enough to drive a bus"), call("Good bye")]
+ 
+@patch("src.lesson4.bus_validator.logger")
+@patch("builtins.print")
+@patch("builtins.input", return_value=23)
+def test_bus_validator_below_23(mock_input, mock_print, mock_logger):
+    bus_validator()
+    assert mock_print.mock_calls == [call("Sorry, you have to wait 1 more years"), call("Good bye")]
+   
+@patch("src.lesson4.bus_validator.logger")
+@patch("builtins.print")
+@patch("builtins.input", return_value="hej")
+def test_bus_validator_not_int(mock_input, mock_print, mock_logger):
+    bus_validator()
+    assert mock_print.mock_calls == [call("You need to enter a number"), call("Good bye")]
